f9cf09fe4199ce705759b40f2fb4f2a1ca8a9ba7
Brenton Lian <brenton.lian@gmail.com>
2026-03-05 23:25:55 -0500

    add utilc utilh to part 3

--- hwsec-course/lab-cacheattacks/Part3-CTF/util.h
+++ hwsec-course/lab-cacheattacks/Part3-CTF/util.h
@@ -1,4 +1,35 @@
+Conversation opened. 2 messages. All messages read.
 
+Skip to content
+Using Gmail with screen readers
+
+Fwd:
+Inbox
+Summarize this email
+
+Apurwa Khanal
+Attachments
+4:26 PM (6 hours ago)
+to me
+
+
+
+---------- Forwarded message ---------
+From: Apurwa Khanal <apurwakhanal@gmail.com>
+Date: Thu, Mar 5, 2026 at 4:14 PM
+Subject:
+To: <naramanly@gmail.com>, <narasimha.cittarusu123@gmail.com>, <shreejalpaudel1234@gmail.com>
+
+
+
+ 11 Attachments
+  •  Scanned by Gmail
+
+Brenton Lian <brenton.lian@gmail.com>
+4:29 PM (6 hours ago)
+to Apurwa
+
+fwd
 // You may only use fgets() to pull input from stdin
 // You may use any print function to stdout to print 
 // out chat messages
@@ -11,6 +42,7 @@
 #include <inttypes.h>
 #include <time.h>
 #include <stdbool.h>
+#include <string.h>
 
 #ifndef UTIL_H_
 #define UTIL_H_
@@ -20,6 +52,15 @@
 
 CYCLES measure_one_block_access_time(ADDR_PTR addr);
 
+// You Should Not Use clflush in your final submission
+// It is only used for debug
 void clflush(ADDR_PTR addr);
 
+char *string_to_binary(char *s);
+char *binary_to_string(char *data);
+
+int string_to_int(char* s);
+
 #endif
+util.h
+Displaying util.h.
