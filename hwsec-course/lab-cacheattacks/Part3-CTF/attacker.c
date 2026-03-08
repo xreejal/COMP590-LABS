77284707c2b25a2abb57e0f194779a2c9f8f880c
Brenton Lian <brenton.lian@gmail.com>
2026-03-08 09:51:47 -0400

    check fix stride for segfault

--- hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
+++ hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
@@ -97,18 +97,22 @@ int main() {
         for(int r = 0; r < REPEATS; r++) {
 
             /* PRIME all sets */
-            for(int set = 0; set < NUM_L2_CACHE_SETS; set++)
-                for(int w = 0; w < WAYS; w++)
+            for(int i = 16; i < NUM_L2_CACHE_SETS-16; i++){
+                int set = perm[i];
+                for(int w = 0; w < WAYS; w++){
                     tmp ^= *eviction_sets[set][w];
+                }
+            }
 
              wait_cycles(sample_wait);
 
             /* PROBE all sets */
-            for(int set = 0; set < NUM_L2_CACHE_SETS; set++) {
+            for(int i = 0; i < NUM_L2_CACHE_SETS; i++) {
+                int set = perm[i];
 
                 uint64_t start = rdtsc();
 
-                for(int w = 0; w < WAYS; w++)
+                for(int w = WAYS-1; w >= 0; w++)
                     tmp ^= *eviction_sets[set][w];
 
                 uint64_t end = rdtsc();
