4e8d69af4aca838ef4acbe3abccdbd36a49c196f
xreejal <shreejalpaudel1234@gmail.com>
2026-03-08 16:31:35 -0400

    new

--- hwsec-course/lab-cacheattacks/Part2-DeadDrop/receiver.c
+++ hwsec-course/lab-cacheattacks/Part2-DeadDrop/receiver.c
@@ -1,29 +1,175 @@
-
-#include"util.h"
-// mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
+#include "util.h"
 #include <sys/mman.h>
+
+#define REGION_BYTES (1 << 21)
+#define L2_WAYS 16
+#define ACCESS_THRESHOLD 125
+#define WARMUP_ROUNDS 10
+#define WARMUP_STRIDE 64
+#define PRIME_DELAY_ITERS 10000000L
+#define COOLDOWN_ITERS 500000000L
+#define DECODE_ROUNDS 5
+#define CONFIRMATION_COUNT 4
+#define TOP_SHOW_COUNT 5
+#define SENTINEL_VALUE -999
+
+static void *map_region(void)
+{
+    void *region = mmap(NULL, REGION_BYTES, PROT_READ | PROT_WRITE,
+                       MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+                       -1, 0);
+
+    if (region == (void *)-1) {
+        perror("mmap error");
+        exit(EXIT_FAILURE);
+    }
+
+    return region;
+}
+
+static void warm_up_cache(void *region, volatile char *probe)
+{
+    *((char *)region) = 1;
+
+    for (int round = 0; round < WARMUP_ROUNDS; round++) {
+        for (uint64_t offset = 0; offset < REGION_BYTES; offset += WARMUP_STRIDE) {
+            *probe = *((char *)region + offset);
+        }
+    }
+}
+
+static void fill_prime_set(void *region, int target_set, volatile char *probe)
+{
+    for (int way = 0; way < L2_WAYS; way++) {
+        uint64_t offset = ((uint64_t)way << 16) | ((uint64_t)target_set << 6);
+        *probe = *((char *)region + offset);
+    }
+}
+
+static int measure_set_latency(void *region, int target_set)
+{
+    uint64_t total_time = 0;
+
+    for (int way = 0; way < L2_WAYS; way++) {
+        uint64_t offset = ((uint64_t)way << 16) | ((uint64_t)target_set << 6);
+        total_time += measure_one_block_access_time((uint64_t)region + offset);
+    }
+
+    return (int)(total_time / L2_WAYS);
+}
+
+static void delay_cycles(long iterations)
+{
+    for (volatile long i = 0; i < iterations; i++) {
+        ;
+    }
+}
 
 int main(int argc, char **argv)
 {
-	// Put your covert channel setup code here
+    (void)argc;
+    (void)argv;
 
-	printf("Please press enter.\n");
+    void *region = map_region();
+    volatile char probe = 0;
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
+    int order[256];
+    int hit_count[256] = {0};
+    int hit_latency_sum[256] = {0};
+    int rounds_seen = 0;
 
-	printf("Receiver now listening.\n");
+    printf("Please press enter.\n");
+    {
+        char ready_buf[2];
+        fgets(ready_buf, sizeof(ready_buf), stdin);
+    }
 
-	bool listening = true;
-	while (listening) {
+    printf("Receiver now listening.\n");
+    warm_up_cache(region, &probe);
 
-		// Put your covert channel code here
+    while (1) {
+        rounds_seen++;
 
-	}
+        for (int i = 0; i < 256; i++) {
+            order[i] = i;
+        }
+        for (int i = 255; i > 0; i--) {
+            int j = rand() % (i + 1);
+            int value = order[i];
+            order[i] = order[j];
+            order[j] = value;
+        }
 
-	printf("Receiver finished.\n");
+        for (int idx = 0; idx < 256; idx++) {
+            int value = order[idx];
+            int set_index = value * 4;
 
-	return 0;
+            fill_prime_set(region, set_index, &probe);
+            delay_cycles(PRIME_DELAY_ITERS);
+
+            int avg_latency = measure_set_latency(region, set_index);
+            if (avg_latency > ACCESS_THRESHOLD) {
+                hit_count[value]++;
+                hit_latency_sum[value] += avg_latency;
+            }
+        }
+
+        printf("Round %d complete\n", rounds_seen);
+
+        int best_count = 0;
+        int best_value = -1;
+        int best_avg_latency = 0;
+
+        for (int v = 0; v < 256; v++) {
+            int avg = hit_count[v] > 0 ? (hit_latency_sum[v] / hit_count[v]) : 0;
+
+            if (hit_count[v] > best_count ||
+                (hit_count[v] == best_count && avg > best_avg_latency)) {
+                best_count = hit_count[v];
+                best_value = v;
+                best_avg_latency = avg;
+            }
+        }
+
+        printf("  Top detections: ");
+        for (int shown = 0; shown < TOP_SHOW_COUNT; shown++) {
+            int top_count = 0;
+            int top_value = -1;
+
+            for (int v = 0; v < 256; v++) {
+                if (hit_count[v] > top_count) {
+                    top_count = hit_count[v];
+                    top_value = v;
+                }
+            }
+
+            if (top_count > 0 && top_value >= 0) {
+                printf("val=%d(cnt=%d) ", top_value, top_count);
+                hit_count[top_value] = SENTINEL_VALUE;
+            }
+        }
+
+        for (int v = 0; v < 256; v++) {
+            if (hit_count[v] == SENTINEL_VALUE) {
+                hit_count[v] = best_count;
+            }
+        }
+        printf("\n");
+
+        if (rounds_seen >= DECODE_ROUNDS && best_count >= CONFIRMATION_COUNT) {
+            printf("\n>>> RECEIVED: %d (detected %d/%d times, avg_lat=%d) <<<\n\n",
+                   best_value, best_count, rounds_seen, best_avg_latency);
+            fflush(stdout);
+
+            for (int v = 0; v < 256; v++) {
+                hit_count[v] = 0;
+                hit_latency_sum[v] = 0;
+            }
+            rounds_seen = 0;
+            delay_cycles(COOLDOWN_ITERS);
+        }
+    }
+
+    munmap(region, REGION_BYTES);
+    return 0;
 }
-
-
--- hwsec-course/lab-cacheattacks/Part2-DeadDrop/sender.c
+++ hwsec-course/lab-cacheattacks/Part2-DeadDrop/sender.c
@@ -1,60 +1,78 @@
-
-#include"util.h"
-// mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
+#include "util.h"
 #include <sys/mman.h>
 
-// TODO: define your own buffer size
-#define BUFF_SIZE (1<<21)
-//#define BUFF_SIZE (2 * 1024 * 1024)
+#define REGION_BYTES (1 << 21)
+#define INPUT_SIZE 128
+#define WARMUP_ROUNDS 5
+#define CACHE_LINE_SIZE 64
+#define SET_SCALE 4
+#define ADDRESS_VARIANTS 32
+
+static void *allocate_shared_region(void)
+{
+    void *region = mmap(NULL, REGION_BYTES, PROT_READ | PROT_WRITE,
+                        MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+                        -1, 0);
+
+    if (region == (void *)-1) {
+        perror("mmap() error");
+        exit(EXIT_FAILURE);
+    }
+
+    return region;
+}
+
+static void warm_up_region(void *region, volatile char *sink)
+{
+    *((char *)region) = 1;
+
+    printf("Warming up...\n");
+    for (int round = 0; round < WARMUP_ROUNDS; round++) {
+        for (uint64_t offset = 0; offset < REGION_BYTES; offset += CACHE_LINE_SIZE) {
+            *sink = *((char *)region + offset);
+        }
+    }
+}
+
+static void keep_sending_value(void *region, int set_index, volatile char *sink)
+{
+    while (1) {
+        for (int way = 0; way < ADDRESS_VARIANTS; way++) {
+            uint64_t offset = ((uint64_t)way << 16) | ((uint64_t)set_index << 6);
+            *sink = *((char *)region + offset);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-  // Allocate a buffer using huge page
-  // See the handout for details about hugepage management
-  void *buf= mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
-  
-  if (buf == (void*) - 1) {
-     perror("mmap() error\n");
-     exit(EXIT_FAILURE);
-  }
-  // The first access to a page triggers overhead associated with
-  // page allocation, TLB insertion, etc.
-  // Thus, we use a dummy write here to trigger page allocation
-  // so later access will not suffer from such overhead.
-   *((char *)buf) = 1; // dummy write to trigger page allocation
+    (void)argc;
+    (void)argv;
 
+    void *region = allocate_shared_region();
+    volatile char sink = 0;
+    warm_up_region(region, &sink);
 
-  // TODO:
-  // Put your covert channel setup code here
- for (int i = 0; i < 256; i++) {
-	_mm_clflush(buf + (i * 4096));
- }
-  printf("Please type a message.\n");
+    printf("Sender ready. Please type a message.\n");
 
-  bool sending = true;
-  while (sending) {
-      char text_buf[128];
-      fgets(text_buf, sizeof(text_buf), stdin);
+    while (1) {
+        char line[INPUT_SIZE];
+        fgets(line, sizeof(line), stdin);
 
-      // TODO:
-      // Put your covert channel code here
-   for (int i = 0; text_buf[i] != '\0' && text_buf[i] != '\n'; i++) {
-    unsigned char c = (unsigned char)text_buf[i];
-    
-    volatile char *addr = (char *)buf + (c * 4096);
-    char dummy = *addr; 
+        int value = atoi(line);
+        if (value < 0 || value > 255) {
+            printf("Please enter a value between 0 and 255\n");
+            continue;
+        }
 
-    for(volatile int sys = 0; sys < 1000000; sys++); 
+        int set_index = value * SET_SCALE;
+        printf("Sending %d (set %d)...\n", value, set_index);
+        printf("Press Ctrl+C when receiver shows the correct value.\n");
 
-    _mm_clflush((void*)addr);
-    
-    printf("Sent: %c (offset %d)\n", c, c * 4096);
-  }
+        keep_sending_value(region, set_index, &sink);
+        printf("Done sending %d\n", value);
+    }
 
-  }
-
-  printf("Sender finished.\n");
-  return 0;
+    munmap(region, REGION_BYTES);
+    return 0;
 }
-
-
