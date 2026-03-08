02c2f5d11a180225d21c620647a8005a31441b94
Brenton Lian <brenton.lian@gmail.com>
2026-03-08 10:23:07 -0400

    fix stride

--- hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
+++ hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
@@ -8,7 +8,7 @@
 #define NUM_L2_CACHE_SETS 1024
 #define WAYS 16
 #define LINE_SIZE 64
-#define STRIDE (NUM_L2_CACHE_SETS * LINE_SIZE * 2)
+#define STRIDE (NUM_L2_CACHE_SETS * LINE_SIZE)
 
 #define REPEATS 2000        // Prime+probe repetitions per round
 #define VOTE_DECAY 200      // Decay old votes every N rounds
@@ -97,7 +97,7 @@ int main() {
         for(int r = 0; r < REPEATS; r++) {
 
             /* PRIME all sets */
-            for(int set = 16; set < NUM_L2_CACHE_SETS-16; set++)
+            for(int i = 16; set < NUM_L2_CACHE_SETS-16; i++)
                 for(int w = 0; w < WAYS; w++)
                     tmp ^= *eviction_sets[set][w];
 
@@ -105,7 +105,7 @@ int main() {
 
             /* PROBE all sets */
             for(int i = 0; i < NUM_L2_CACHE_SETS; i++) {
-                int set = perm[i]
+                int set = perm[i];
 
                 uint64_t start = rdtsc();
 
